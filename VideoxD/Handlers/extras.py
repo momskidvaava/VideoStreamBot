00bda260ed3959503565208b5365fa3d1eb85c31
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-09-15 03:18:57 +0530

    Update extras.py

--- VideoxD/Handlers/extras.py
+++ VideoxD/Handlers/extras.py
@@ -5,32 +5,14 @@ from ..functions import admin_check
 
 
 @bot.on_message(filters.command("vpause") & filters.chat(chat_id))
-async def pause(client, message):
-    if not Calls.is_running:
-        return await message.reply(
-            "There is Nothing Streaming, What Shall be Paused ⏸ ?"
-        )
-    admins = await admin_check(client, message)
-    if message.from_user.id not in admins:
-        return await message.reply(
-            "You Dont Have Sufficient Permissions!,(Manage Video Chats)"
-        )
-    if Calls.is_paused:
-        return await message.reply("The Video is already paused!")
+async def pause(client, message):  
+    admins = await admin_check(client, message)   
     await Calls.set_pause(True)
-    return await message.reply("The Video Has Been Paused ⏸ Successfully!")
+    
 
 
 @bot.on_message(filters.command("vresume") & filters.chat(chat_id))
-async def resume(client, message):
-    if not Calls.is_running:
-        return await message.reply("There is Nothing Streaming!")
-    admins = await admin_check(client, message)
-    if message.from_user.id not in admins:
-        return await message.reply(
-            "You Dont Have Sufficient Permissions,(Manage Video Chat)"
-        )
-    if not Calls.is_paused:
-        return await message.reply("The Video is already playing!")
+async def resume(client, message):   
+    admins = await admin_check(client, message)   
     await Calls.set_pause(False)
-    return await message.reply("The Video Has Been Resumed ▶️ Successfully!")
+    
